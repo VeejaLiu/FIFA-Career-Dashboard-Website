1501cd92f7947b6b8b2b527025cc02034a0a0df0
VeejaLiu <veejaliu@gmail.com>
2024-09-10 19:03:32 +0800

    add new button

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,22 @@
 import './App.css';
-import { Carousel, Space, Typography } from '@douyinfe/semi-ui';
+import { Button, Carousel } from '@douyinfe/semi-ui';
 import easy_to_use_img from './assets/easy_to_use.png';
 import player_list_page_img from './assets/player_list_page.png';
 import player_trends_page_img from './assets/player_trends_page.png';
 
 function App() {
-  const { Title, Paragraph } = Typography;
-
-  const renderLogo = () => {
-    return (
-      <img
-        src="https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/semi_logo.svg"
-        alt="semi_logo"
-        style={{ width: 87, height: 31 }}
-      />
-    );
-  };
-
   const imgList = [
     easy_to_use_img,
     player_list_page_img,
     player_trends_page_img,
   ];
 
-  const textList = [
-    [
-      'Semi 设计管理系统',
-      '从 Semi Design，到 Any Design',
-      '快速定制你的设计系统，并应用在设计稿和代码中',
-    ],
-    [
-      'Semi 物料市场',
-      '面向业务场景的定制化组件，支持线上预览和调试',
-      '内容由 Semi Design 用户共建',
-    ],
-    [
-      'Semi 设计/代码模板',
-      '高效的 Design2Code 设计稿转代码',
-      '海量 Figma 设计模板一键转为真实前端代码',
-    ],
-  ];
   return (
     <>
       <Carousel
         style={{
           width: '100%',
-          height: '400px',
+          height: '500px',
         }}
         theme={'primary'}
         autoPlay={false}
@@ -59,29 +30,20 @@ function App() {
                 backgroundImage: `url('${src}')`,
               }}
             >
-              <Space
-                vertical
-                align="start"
-                spacing="medium"
+              <Button
+                theme={'borderless'}
                 style={{
-                  position: 'absolute',
-                  top: '100px',
-                  left: '100px',
+                  backgroundColor: '#000',
+                  color: '#fff',
+                  borderRadius: '10px',
+                }}
+                onClick={() => {
+                  console.log('Get Started');
+                  window.open('https://app.fccareer.top', '_blank');
                 }}
               >
-                {renderLogo()}
-                <Title heading={2} style={{ color: '#1C1F23' }}>
-                  {textList[index][0]}
-                </Title>
-                <Space vertical align="start">
-                  <Paragraph style={{ color: '#1C1F23' }}>
-                    {textList[index][1]}
-                  </Paragraph>
-                  <Paragraph style={{ color: '#1C1F23' }}>
-                    {textList[index][2]}
-                  </Paragraph>
-                </Space>
-              </Space>
+                Get Started
+              </Button>
             </div>
           );
         })}
