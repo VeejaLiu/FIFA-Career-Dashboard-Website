3c40d91be7821d63e77d24665d969fa0d3470a84
VeejaLiu <veejaliu@gmail.com>
2024-09-11 19:12:32 +0800

    Change the style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import './App.css';
-import { Button, Carousel, Space } from '@douyinfe/semi-ui';
+import { Button, Carousel, Layout, Space } from '@douyinfe/semi-ui';
 import easy_to_use_img from './assets/easy_to_use.png';
 import player_list_page_img from './assets/player_list_page.png';
 import player_trends_page_img from './assets/player_trends_page.png';
+import './App.css';
+
+const { Header, Footer, Content } = Layout;
+
+function goToApp() {
+  window.open('https://app.fccareer.top', '_blank');
+}
 
 function App() {
   const imgList = [
@@ -12,82 +18,95 @@ function App() {
   ];
 
   return (
-    <>
-      <Space>
-        <div>
-          <h1>FC Career</h1>
-          <p>Track your players' progress</p>
-        </div>
-        <div>
-          <Button
-            theme={'borderless'}
+    <Layout className="components-layout-demo">
+      <Header style={{ height: '61px', backgroundColor: '#151616' }}>
+        <Space>
+          <div>
+            <span style={{ color: '#FFF' }}>FC</span>
+            <span style={{ color: '#FFF' }}>Career</span>
+            <span style={{ color: '#FFF' }}>.top</span>
+          </div>
+
+          <div
+            className="my-button"
             onClick={() => {
-              console.log('Get Started');
-              window.open('https://app.fccareer.top', '_blank');
+              goToApp();
             }}
           >
             Go to App
+          </div>
+        </Space>
+      </Header>
+
+      <Content style={{ width: '100%' }}>
+        <Carousel
+          style={{
+            width: '100%',
+            height: '500px',
+          }}
+          theme={'light'}
+          autoPlay={false}
+        >
+          {imgList.map((data, index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  backgroundSize: 'cover',
+                  backgroundImage: `url('${data.img}')`,
+                }}
+              ></div>
+            );
+          })}
+        </Carousel>
+        <Space
+          align={'center'}
+          style={{
+            height: '100px',
+            width: '100%',
+            color: '#fafafa',
+            backgroundColor: '#323232',
+            padding: '0 auto',
+            margin: '0 auto',
+          }}
+        >
+          <h1
+            style={{
+              fontSize: '1.5rem',
+              margin: '0 auto',
+            }}
+          >
+            Dear Manager, Start Tracking Your Players' Progress!
+          </h1>
+          <Button
+            className="my-button"
+            onClick={() => {
+              goToApp();
+            }}
+          >
+            Start Now
           </Button>
-        </div>
-      </Space>
-      <Carousel
+        </Space>
+      </Content>
+      <Footer
         style={{
-          width: '100%',
-          height: '500px',
-        }}
-        theme={'primary'}
-        autoPlay={false}
-      >
-        {imgList.map((data, index) => {
-          return (
-            <div
-              key={index}
-              style={{
-                backgroundSize: 'cover',
-                backgroundImage: `url('${data.img}')`,
-              }}
-            ></div>
-          );
-        })}
-      </Carousel>
-      <Space
-        align={'center'}
-        style={{
-          height: '100px',
-          width: '100%',
+          height: '200px',
+          backgroundColor: '#1e1f1f',
           color: '#fff',
-          backgroundColor: '#222',
-          padding: '0 auto',
         }}
       >
-        <h1
+        <Space
+          vertical
+          align={'center'}
           style={{
-            fontSize: '1.5rem',
-            margin: '0 auto',
+            height: '100%',
+            width: '100%',
           }}
         >
-          Dear Manager, Start Tracking Your Players' Progress!
-        </h1>
-        <Button
-          theme={'borderless'}
-          style={{
-            backgroundColor: '#2962f1',
-            borderRadius: '10px',
-            height: '60px',
-            width: '300px',
-            fontSize: '2rem',
-            margin: '0 auto',
-            color: '#fff',
-          }}
-          onClick={() => {
-            console.log('Get Started');
-            window.open('https://app.fccareer.top', '_blank');
-          }}
-        >
-          Start Now
-        </Button>
-      </Space>
-    </>
+          CopyRight © 2024 FC-Career-Dashboard
+        </Space>
+      </Footer>
+    </Layout>
   );
 }
 
