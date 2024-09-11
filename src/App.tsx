6674a4fb811ea36d9fafd757c77a8b30bf7dbe8a
VeejaLiu <veejaliu@gmail.com>
2024-09-11 17:36:56 +0800

    Change the style

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import './App.css';
-import { Button, Carousel } from '@douyinfe/semi-ui';
+import { Button, Carousel, Space } from '@douyinfe/semi-ui';
 import easy_to_use_img from './assets/easy_to_use.png';
 import player_list_page_img from './assets/player_list_page.png';
 import player_trends_page_img from './assets/player_trends_page.png';
 
 function App() {
   const imgList = [
-    easy_to_use_img,
-    player_list_page_img,
-    player_trends_page_img,
+    { img: easy_to_use_img, text: 'Easy to use' },
+    { img: player_list_page_img, text: 'Player List Page' },
+    { img: player_trends_page_img, text: 'Player Trends Page' },
   ];
 
   return (
     <>
+      <Space>
+        <div>
+          <h1>FC Career</h1>
+          <p>Track your players' progress</p>
+        </div>
+        <div>
+          <Button
+            theme={'borderless'}
+            onClick={() => {
+              console.log('Get Started');
+              window.open('https://app.fccareer.top', '_blank');
+            }}
+          >
+            Go to App
+          </Button>
+        </div>
+      </Space>
       <Carousel
         style={{
           width: '100%',
@@ -21,33 +38,55 @@ function App() {
         theme={'primary'}
         autoPlay={false}
       >
-        {imgList.map((src, index) => {
+        {imgList.map((data, index) => {
           return (
             <div
               key={index}
               style={{
                 backgroundSize: 'cover',
-                backgroundImage: `url('${src}')`,
+                backgroundImage: `url('${data.img}')`,
               }}
-            >
-              <Button
-                theme={'borderless'}
-                style={{
-                  backgroundColor: '#000',
-                  color: '#fff',
-                  borderRadius: '10px',
-                }}
-                onClick={() => {
-                  console.log('Get Started');
-                  window.open('https://app.fccareer.top', '_blank');
-                }}
-              >
-                Get Started
-              </Button>
-            </div>
+            ></div>
           );
         })}
       </Carousel>
+      <Space
+        align={'center'}
+        style={{
+          height: '100px',
+          width: '100%',
+          color: '#fff',
+          backgroundColor: '#222',
+          padding: '0 auto',
+        }}
+      >
+        <h1
+          style={{
+            fontSize: '1.5rem',
+            margin: '0 auto',
+          }}
+        >
+          Dear Manager, Start Tracking Your Players' Progress!
+        </h1>
+        <Button
+          theme={'borderless'}
+          style={{
+            backgroundColor: '#2962f1',
+            borderRadius: '10px',
+            height: '60px',
+            width: '300px',
+            fontSize: '2rem',
+            margin: '0 auto',
+            color: '#fff',
+          }}
+          onClick={() => {
+            console.log('Get Started');
+            window.open('https://app.fccareer.top', '_blank');
+          }}
+        >
+          Start Now
+        </Button>
+      </Space>
     </>
   );
 }
