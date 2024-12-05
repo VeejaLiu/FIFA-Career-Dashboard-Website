a194f8156f22967507fa2b32f21689ed9c1714e1
VeejaLiu <veejaliu@gmail.com>
2024-12-05 09:58:05 +0800

    add postcss.config.js     tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Tailwind CSS 将扫描这些文件以生成样式
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+//
